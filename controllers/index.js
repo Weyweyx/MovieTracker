520fe178345df14437d05acf6a446d385c755f08
napling <matthewcellis98@gmail.com>
2024-10-03 21:15:37 -0500

    added api and routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
-const habitRoutes = require('./habitController');
-const userRoutes = require('./userController');
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeController');
 
 router.use('/', homeRoutes);
-router.use('/habits', habitRoutes);
-router.use('/users', userRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
