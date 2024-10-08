e719df2708cb0f6ae5c4fc1c9d2fa567f988d299
Caroline Bost <cbost26@gmail.com>
2024-10-07 20:37:55 -0400

    edited folder structure and helper js

--- tools/models/movies.js
+++ tools/models/movies.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../tools/config/connection');
 
 class Movie extends Model {}
 
