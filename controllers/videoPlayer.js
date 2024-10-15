3852911f0372704a659ae25d792d90dbaa045136
Zakary Johnson <zakary.johnson575@gmail.com>
2024-10-14 19:10:13 -0500

    added the API key to the .env and fixed the quotes on the src attribute to back tics

--- controllers/videoPlayer.js
+++ controllers/videoPlayer.js
@@ -1,8 +1,10 @@
 const { Movie } = require("../models/Movie");
 const video = document.querySelector("video");
+require('dotenv').config();
+
 
 function fetchYouTubeVideos(query) {
-  const apiKey = "AIzaSyAZQxBxJRTVW5bNFyFSHAj-xF8GBWF3NQ4";
+  const apiKey = process.env.youtubeAPI ;
   const baseUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet";
   const url =
     baseUrl +
@@ -29,7 +31,7 @@ function fetchYouTubeVideos(query) {
 
 function setVideo(videos) {
   const source = document.createElement("source");
-  source.setAttribute(src,"https://www.youtube.com/embed/${videos[0].id.videoId}?autoplay=1&mute=1")
+  source.setAttribute(src,`https://www.youtube.com/embed/${videos[0].id.videoId}?autoplay=1&mute=1`)
   source.setAttribute(type, "video/youtube")
 
   video.append(source)
