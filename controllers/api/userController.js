6a1ee7bf46ced47c406ac4f353f118f85ae30490
napling <matthewcellis98@gmail.com>
2024-10-16 17:08:22 -0500

    updated login routes to be able to login

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -45,18 +45,14 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({
+      where: {
+        email: req.body.email,
+        password: req.body.password
+      }
+    });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
