88468a212f84e5c9c6cc27030fee9ca316975dd8
napling <matthewcellis98@gmail.com>
2024-10-16 21:04:47 -0500

    removed comments and empty files

--- models/index.js
+++ models/index.js
@@ -3,15 +3,6 @@ const Watchlist = require('./Watchlist');
 const Movie = require('./Movie');
 const Review = require('./Review');
 
-// User.hasOne(Watchlist, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE'
-// });
-
-// Watchlist.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
-
 User.hasMany(Movie, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
@@ -20,37 +11,5 @@ User.hasMany(Movie, {
 Movie.belongsTo(User, {
     foreignKey: 'user_id'
 })
-
-// User.belongsToMany(Movie, {
-//     through: {
-//         model: Watchlist,
-//         unique: false
-//     },
-//     as: 'movies_to_watch'
-// })
-
-// Movie.belongsToMany(User, {
-//     through: {
-//         model: Watchlist,
-//         unique: false
-//     },
-//     as: 'movie_fans'
-// })
-
-// User.hasMany(Review, {
-//     foreignKey: 'user_id'
-// })
-
-// Review.belongsTo(User, {
-//     foreignKey: 'user_id'
-// })
-
-// Movie.hasMany(Review, {
-//     foreignKey: 'movie_id'
-// })
-
-// Review.belongsTo(Movie, {
-//     foreignKey: 'movie_id'
-// })
 
 module.exports = { User, Watchlist, Movie, Review };
