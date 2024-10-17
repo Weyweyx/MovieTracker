bc037489dba6fc1bbbd188ec14d1eb6359d93bf1
napling <matthewcellis98@gmail.com>
2024-10-17 17:10:23 -0500

    added the checkpassword to let bcrypt hassh the password

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -18,14 +18,18 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({
-      where: {
-        email: req.body.email,
-        password: req.body.password
-      }
-    });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
@@ -38,7 +42,6 @@ router.post('/login', async (req, res) => {
 
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
   } catch (err) {
     res.status(400).json(err);
   }
